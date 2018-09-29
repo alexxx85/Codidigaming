443a0604badefbf31ddcc9346f6126db7f85b142
alexxx85 <a.c.peribanez@gmail.com>
2018-09-29 19:20:22 +0200

    first commit

---
+++ codingGamifier/src/main/MainClass.java
@@ -0,0 +1,28 @@
+/*
+ * Style of this code is Sage Code style. For further information on this style please
+ * visit the following link: https://sagecode.net/sage-code-style 
+ */
+
+package main;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String []args) {
+		
+		// Variables //
+		
+		Scanner scan = new Scanner(System.in);	
+		
+		//End of Variables //
+		
+		System.out.println("Welcome to codifigaming!");
+		
+		
+		
+		
+		
+	}
+	
+}
